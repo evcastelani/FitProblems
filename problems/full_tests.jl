58541de8e3eb4e0c89bf87d29eee9d9888bfd70d
evcastelani <emersonvitor@gmail.com>
2022-09-23 16:19:04 -0400

    firsts tests done!

--- problems/full_tests.jl
+++ problems/full_tests.jl
@@ -7,9 +7,10 @@ filename = "list.txt"
 methods = ["LMlovo","NelderMead","SimulatedAnnealing","ParticleSwarm","RAFF"]
 list = String.(readdlm(filename))
 io = open("log_read_erros.txt", "w");
+c = 1.0e-9
 for methodname in methods
     # colocar em dataframe
-    df = DataFrame(prob = String[], error= Float64[],minimum=Float64[],feval=Float64[],niter=Int64[],convergence=Bool[], minimum_PT = Float64[], maximum_PT = Float64[], mean_PT = Float64[], median_PT = Float64[])
+    df = DataFrame(prob = String[],dim=Int64[],npts=Int64[],nout=Int64[], error= Float64[],minimum=Float64[],feval=Float64[],niter=Int64[],convergence=Bool[], minimum_PT = Float64[], maximum_PT = Float64[], mean_PT = Float64[], median_PT = Float64[], memory = Int64[],alloc = Int64[])
     #list = ["parabola_-0.5_-9.0_0.0_650_143.csv"]
     for file in list
         local prob = load_problem(file)
@@ -19,7 +20,7 @@ for methodname in methods
             println(" 🕥 Performing benchmark! Please wait!")
             local b = @benchmark solve($(prob),zeros($(prob.dim)),$(methodname))
             display(b)
-            push!(df,[file,norm(prob.solution-s.solution,Inf),s.minimum,s.feval,s.niter,s.status,minimum(b.times),maximum(b.times),mean(b.times),median(b.times)])
+            push!(df,[file,prob.dim,prob.npts,prob.nout,norm(prob.solution-s.solution,Inf),s.minimum,s.feval,s.niter,s.status,c*minimum(b.times),c*maximum(b.times),c*mean(b.times),c*median(b.times),b.memory,b.allocs])
         catch
             println(" 💣 Some read error in problem $(file) occurred")
             write(io," Error reading $(file) using $(methodname) occurred \n")
